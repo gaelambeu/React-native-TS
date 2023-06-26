fdd5cfd617c6fc736f42477860ef64845e160f6d
Gael16062003 <ambeugael128@gmail.com>
2023-06-26 20:16:56 +0200

    new folder

---
+++ My_Notepad/App.tsx
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <TextInput  multiline={true} style={styles.TextInput} />
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  TextInput: {
+    backgroundColor: "#ffb70342", 
+    width:"100%",
+     height:200
+  }
+});
